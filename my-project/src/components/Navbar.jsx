2fc5f9eced72b43c74999ee27de19650fdca2182
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-14 23:49:37 +0300

    Added: social media icons

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <nav className=" bg-red-900 mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <img src={logo} alt="logo" />
       </div>
