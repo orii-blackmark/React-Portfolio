782e0e591ee128e2e0192d7afcbdc3f2232c7f0d
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-14 23:20:10 +0300

    Modified: Navbar.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import logo from "../assets/KevinRushLogo.png";
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaTwitter />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
